881f6ef2f3df39ac589ec7727b92ca0633b4c044
Max Smiley <maxsmiley0@gmail.com>
2020-09-20 14:36:34 -0700

    strawman 2.0?

---
+++ strawberrymangoes/strawberrymangoes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  strawberrymangoes
+//
+//  Created by Max Smiley on 9/20/20.
+//  Copyright © 2020 Max Smiley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
