4216e93d2899e246f0461781c94e52bb205763e9
Max Smiley <maxsmiley0@gmail.com>
2020-10-04 15:23:48 -0700

    Added game files and started storyboarding
    The game class will have a searcher, and will be responsible for taking in user input moves and spitting out computer recommended moves. Essentially, it is the bridge between human and computer.

---
+++ strawberrymangoes/strawberrymangoes/Game.cpp
@@ -0,0 +1,9 @@
+//
+//  Game.cpp
+//  strawberrymangoes
+//
+//  Created by Max Smiley on 10/4/20.
+//  Copyright © 2020 Max Smiley. All rights reserved.
+//
+
+#include "Game.h"
---
+++ strawberrymangoes/strawberrymangoes/Game.h
@@ -0,0 +1,35 @@
+//
+//  Game.hpp
+//  strawberrymangoes
+//
+//  Created by Max Smiley on 10/4/20.
+//  Copyright © 2020 Max Smiley. All rights reserved.
+//
+
+#ifndef Game_h
+#define Game_h
+
+class Game
+{
+public:
+    //constructor
+    //destructor
+    
+    //"play"
+    /*
+     loops while game over, cin
+     some kind of vet function
+     */
+    //reset game
+    
+    //accessors for movegen, searcher, board
+    
+private:
+    //Checks when game is over
+    //Start fen
+    //vet fnct
+    //searcher
+    //player side
+};
+
+#endif /* Game_h */
